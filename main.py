e659718ae71675def51eb8b62787e86941f5a68c
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2023-08-21 16:16:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ Technical Skills: Rate the candidate's grasp of technical concepts and their abi
 Problem-Solving Abilities: Gauge the candidate's approach to problem-solving, including their methodology and creative thinking.Offer reasoning behind the score provided, considering how well the solution addressed the question's context.
 Creativity: Assess the originality and innovative thinking demonstrated in the response.Provide rationale for the score assigned, focusing on the uniqueness of the candidate's approach.
 
-Assign scores for each criterion on a scale from 1 to 10.Be strict with the scores.
+Assign scores for each criterion on a scale from 1 to 10.
 
 Response: ###{answer}###
 
@@ -163,7 +163,7 @@ with st.sidebar:
 
 
 if not job_description:
-    st.info("Please enter a job description")
+    st.info("Please enter a job description in the Sidebar!")
     st.stop()
 
 if "disabled" not in st.session_state:
